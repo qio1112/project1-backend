c97bc5492b1f2a417f764b35160935b18635aaec
NicksComingUp <remember.marsman@gmail.com>
2019-06-29 09:37:36 -0400

    adding user login routes

--- src/main/java/com/routes/AuthRoutes.java
+++ src/main/java/com/routes/AuthRoutes.java
@@ -1,9 +1,12 @@
 package com.routes;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.User;
 
 
 /**
@@ -11,8 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Nicholas Marsden
  *
  *
- *@ResponseBody - use http messages converters to convert the return value to http response body
+ *
  */
+
+/*
+ * @ResponseBody 
+ * - use http messages converters to convert the return value to http response body
+ * 
+ * 
+ * @ModelAttribute("SpringWeb")User user
+ * the above method was said to be able to use on forms 
+ * dont think it would be compatible with anglare framework,
+ * its a good fit for the jsp though
+*/
 @RestController
 @RequestMapping(value="/auth")
 public class AuthRoutes {
@@ -22,16 +36,23 @@ public class AuthRoutes {
 		return "auth test successfull";
 
 	}
+/* 
+ * takes the user information does a validation,
+ * store it in the db table users 
+*/
 	@RequestMapping(value="/register", method= RequestMethod.POST)
-	public void registerRoute() { // signup
+	public void registerRoute(@RequestBody User user) { // signup
 		System.out.println("register user");
 	}
+	
 	@RequestMapping(value="/signin",  method= RequestMethod.POST)
-	public void signinRoute() {
+	public void signinRoute(@RequestBody User user) {
 		System.out.println("signin user");
 	}
+	// remove the token so no one else can use it.
 	@RequestMapping(value="/logout", method= RequestMethod.GET)
-	public void logout() {
+	public void logout(String token) {
 		System.out.println("logout user");
 	}
 }
+
