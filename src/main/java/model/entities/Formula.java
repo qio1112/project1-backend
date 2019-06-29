10fa2448c57b4a434781d40c5f777cfb7a5257aa
NicksComingUp <remember.marsman@gmail.com>
2019-06-29 12:16:53 -0400

    formula class to get from the submition of formula table

---
+++ src/main/java/model/entities/Formula.java
@@ -0,0 +1,24 @@
+package model.entities;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Formula {
+
+	int product_id;
+	ArrayList<String> column_added;
+	ArrayList<String> column_delete;
+	ArrayList<String> resource_code;
+	
+	ArrayList<Map<String, String>> formula;
+	ArrayList<Map<String, ArrayList<Object>>> columns;
+	ArrayList<String> column_type;
+	
+	
+	/*
+	 * Methods to get Each column from the user
+	 * AND 
+	 * Mehtods to get Each Column from the database
+	 * 
+	 * */
+}
